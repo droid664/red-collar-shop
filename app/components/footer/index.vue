84855af6831f38b8d409fe42011b458c44d02de6
Droid664 <iskandarov.murat@gmail.com>
2024-02-17 17:35:30 +0500

    Добавил подвал

---
+++ app/components/footer/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <footer class="footer">
+        <div class="container">
+            <div class="footer__inner">
+                <nuxt-link to="/" class="footer__shop">nonameshop©2024</nuxt-link>
+                <span class="footer__icon">
+                    <UIIcon id="icon-star" />
+                </span>
+                <a
+                    class="footer__develop"
+                    target="_blank"
+                    href="https://redcollar.ru/"
+                    rel="noopener"
+                >
+                    <img src="~/assets/img/red-collar-logo.svg" alt="Made in collar" />
+                </a>
+            </div>
+        </div>
+    </footer>
+</template>
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,6 +1,14 @@
 <template>
     <div class="default-layout">
-        <slot></slot>
+        <div class="site">
+            <div class="site-top">
+                <slot></slot>
+            </div>
+            <div class="site-bottom">
+                <Footer />
+            </div>
+        </div>
+
         <Basket />
         <UISprite />
     </div>
